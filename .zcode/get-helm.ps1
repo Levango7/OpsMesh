c84bd6cd685d06eac221416e03a7e30fcc470533
Levango7 <Levango7@users.noreply.github.com>
2026-09-01 05:56:44 +0800

    chore: 移除误提交的 helm 下载临时脚本

--- .zcode/get-helm.ps1
+++
@@ -1,4 +0,0 @@
-$ProgressPreference='SilentlyContinue'
-Invoke-WebRequest -Uri 'https://get.helm.sh/helm-v3.14.4-windows-amd64.zip' -OutFile "$env:TEMP\helm.zip"
-Expand-Archive "$env:TEMP\helm.zip" -DestinationPath "$env:TEMP\helm-bin" -Force
-& "$env:TEMP\helm-bin\windows-amd64\helm.exe" version
